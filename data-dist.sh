13a750217dee0e85d4436f2400573d4221d8d820
jesse <jessedp@gmail.com>
2018-07-21 11:53:15 -0400

    quiet the zipping

--- data-dist.sh
+++ data-dist.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd data
-/usr/bin/zip timegeopack.sqlite3.zip timegeopack.sqlite3
+/usr/bin/zip -q timegeopack.sqlite3.zip timegeopack.sqlite3
 cd cities
-/usr/bin/zip timegeopack.topcities.zip *
+/usr/bin/zip -q timegeopack.topcities.zip *
